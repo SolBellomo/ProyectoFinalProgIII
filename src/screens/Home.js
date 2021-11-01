0e96cdf5a6a76e3e803633d27f54bf7a16faef9e
unknown <sbello@udesa.edu.ar>
2021-11-01 11:16:04 -0300

    login,registro,menu

---
+++ src/screens/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {Text, View} from 'react-native'; 
+
+class Home extends Component {
+
+  constructor() {
+    super();
+    this.setState
+}
+    render(){
+        return (
+            
+              <View>
+                    <Text>Home</Text>
+              </View>
+          
+
+          )
+        }
+    }
+  
+    export default Home;
