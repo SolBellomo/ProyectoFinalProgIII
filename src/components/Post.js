3f87773d2807fde7db92c4c185dd3b915892cf83
Maite Galindez <maitegalindez@Maites-MacBook-Air.local>
2021-11-24 15:22:45 -0300

    post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,209 +1,250 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { Text, View, TouchableOpacity,  StyleSheet, Image, ActivityIndicator, Modal, FlatList } from 'react-native';
-import firebase from 'firebase';
-import { db, auth } from '../firebase/config';
-import CommentForm from '../components/CommentForm';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  Modal,
+  FlatList,
+} from "react-native";
+import firebase from "firebase";
+import { db, auth } from "../firebase/config";
+import CommentForm from "../components/CommentForm";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {} from '@fortawesome/free-brands-svg-icons';
-import { faHeart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-brands-svg-icons";
+import { faHeart, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 class Post extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            likes: 0,
-            myLike: false,
-            onComment: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      myLike: false,
+      onComment: false,
+      comments: [],
+      showModal: false,
+    };
+  }
+  componentDidMount() {
+    this.reciveLikes();
+  }
+
+  reciveLikes() {
+    let likes = this.props.postData.data.likes;
+    if (likes) {
+      this.setState({
+        likes: likes.length,
+      });
     }
- componentDidMount(){
-     this.reciveLikes();
- }
-
-    reciveLikes(){
-        let likes = this.props.postData.data.likes;
-        if(likes){
-            this.setState({
-                likes: likes.length,
-
-            })
-        } if (likes.includes(auth.currentUser.email)
-        ){
-            this.setState({
-                myLike: true,
-            })
-        }
+    if (likes.includes(auth.currentUser.email)) {
+      this.setState({
+        myLike: true,
+      });
     }
-       
+  }
 
-    
-
-    
-    likePosts(){
-        let post = db.collection('posts').doc(this.props.postData.id);
-        post.update({
-            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
-        })
-        .then(() =>{
-            this.setState({
-                likes: this.state.likes +1,
-                myLike: true
-            })
-            console.log('likeado')
-        })
-        .catch((error) => {
-            console.error("Error updating document: ", error);
+  likePosts() {
+    let post = db.collection("posts").doc(this.props.postData.id);
+    post
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(
+          auth.currentUser.email
+        ),
+      })
+      .then(() => {
+        this.setState({
+          likes: this.state.likes + 1,
+          myLike: true,
         });
+        console.log("likeado");
+      })
+      .catch((error) => {
+        console.error("Error updating document: ", error);
+      });
+  }
 
-    }
-    
-    unLikePosts(){
-        let post = db.collection('posts').doc(this.props.postData.id);
-        post.update({
-            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
-        })
-        .then(() =>{
-            this.setState({
-                likes: this.state.likes -1,
-                myLike: false
-            })
-            console.log('deslikeado')
-        })
-        .catch((error) => {
-            console.error("Error updating document: ", error);
+  unLikePosts() {
+    let post = db.collection("posts").doc(this.props.postData.id);
+    post
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(
+          auth.currentUser.email
+        ),
+      })
+      .then(() => {
+        this.setState({
+          likes: this.state.likes - 1,
+          myLike: false,
         });
+        console.log("deslikeado");
+      })
+      .catch((error) => {
+        console.error("Error updating document: ", error);
+      });
+  }
 
-    }
-    
-    deletePost(){
-        db.collection('posts').doc(this.props.postData.id).delete().then(() => {
-            console.log("Document successfully deleted!");
-        })
-    }
+  deletePost() {
+    db.collection("posts")
+      .doc(this.props.postData.id)
+      .delete()
+      .then(() => {
+        console.log("Document successfully deleted!");
+      });
+  }
 
+  showModal() {
+    this.setState({
+      showModal: true,
+    });
+  }
 
+  closeModal() {
+    this.setState({
+      showModal: false,
+    });
+  }
 
-    render(){
-        console.log(this.props.postData) 
-        return(
+  render() {
+    console.log(this.props.postData);
+    console.log(this.state.comments);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.user}>
+          <FontAwesomeIcon icon={faUserCircle} />{" "}
+          {this.props.postData.data.ownerNik}
+        </Text>
 
-            
-            
-             
-                <View style={styles.container}>
-                 
-                    
-                    <Text style={styles.user} ><FontAwesomeIcon icon={faUserCircle} /> {this.props.postData.data.ownerNik}</Text>
-                    
-                    <Image 
-                        style={{flex: 1, width: 300, height:250}}
-                        source={{uri: this.props.postData.data.photo}}
-                    />
-                     
-                    {!this.state.myLike ?
+        <Image
+          style={{ flex: 1, width: 300, height: 250 }}
+          source={{ uri: this.props.postData.data.photo }}
+        />
 
-                    <TouchableOpacity style={styles.likeButton} onPress={()=>this.likePosts()}>
-                        <Text style={{color:'black',}}><FontAwesomeIcon icon={faHeart}/></Text>
-                        <Text> {this.state.likes} </Text>
+        {!this.state.myLike ? (
+          <TouchableOpacity
+            style={styles.likeButton}
+            onPress={() => this.likePosts()}
+          >
+            <Text style={{ color: "black" }}>
+              <FontAwesomeIcon icon={faHeart} />
+            </Text>
+            <Text> {this.state.likes} </Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={styles.likeButton}
+            onPress={() => this.unLikePosts()}
+          >
+            <Text style={{ color: "red" }}>
+              <FontAwesomeIcon icon={faHeart} />
+            </Text>
+            <Text> {this.state.likes} </Text>
+          </TouchableOpacity>
+        )}
 
-                    </TouchableOpacity>:
+        <Text style={styles.title}> {this.props.postData.data.title}</Text>
+        <Text style={styles.description}>
+          {" "}
+          {this.props.postData.data.description}
+        </Text>
 
-                    <TouchableOpacity style={styles.likeButton} onPress={()=>this.unLikePosts()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.deletePost()}
+        >
+          <Text style={styles.textButton}>Borrar post</Text>
+        </TouchableOpacity>
 
-                        <Text style={{color:'red',}}><FontAwesomeIcon icon={faHeart}/></Text>
-                        <Text> {this.state.likes} </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.showModal()}
+        >
+          <Text style={styles.textButton}>Ver más</Text>
+        </TouchableOpacity>
 
-                    </TouchableOpacity>}
+        {this.state.showModal ? (
+          <Modal
+            visible={this.state.showModal}
+            animationType="slide"
+            transparent={false}
+            style={styles.comentarios}
+          >
+            <Text>Comentarios</Text>
 
-                    <Text style={styles.title}> {this.props.postData.data.title}</Text>
-                    <Text style={styles.description}> {this.props.postData.data.description}</Text>
-
-                    <TouchableOpacity style={styles.button} onPress={()=>this.deletePost()}>
-                        <Text style={styles.textButton}>
-                        Borrar post
-                        </Text>    
-                    </TouchableOpacity>
-
-                    <Modal visible={false} animationType="slide" transparent={false} style={styles.comentarios}>
-                        <Text>Comentarios</Text>
-
-                        <CommentForm postId={this.props.postData.id} />
-                            
-                        <FlatList 
-                            data={this.state.comments}
-                            keyExtractor={(com) => com.id}            
-                            renderItem = {({item})=> <CommentForm postId={item}/>}
-                        />
-                    </Modal>
-                   
-                    
-                
-                </View>
-            
-        )
-    }
+            <CommentForm
+              postId={this.props.postData.id}
+              comments={this.props.postData.data.comments}
+              close={() => this.closeModal()}
+            />
+          </Modal>
+        ) : (
+            null
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems: 'center',
-        borderColor: '#DCDCDC',
-        borderWidth: 2,
-        borderStyle: 'solid',
-        padding: 50,
-        flex: 2,
-        display: 'flex',
-        marginBottom: 10,
-        borderRadius: 12,
-    },
+  container: {
+    alignItems: "center",
+    borderColor: "#DCDCDC",
+    borderWidth: 2,
+    borderStyle: "solid",
+    padding: 50,
+    flex: 2,
+    display: "flex",
+    marginBottom: 10,
+    borderRadius: 12,
+    alignSelf: "center",
+  },
 
-    comentarios: {
-        borderColor: 'transparent',
-        alignSelf: 'center',
-        marginTop: '20%',
-    },
+  comentarios: {
+    borderColor: "transparent",
+    alignSelf: "center",
+    marginTop: "20%",
+  },
 
-    title: {
-        fontWeight: 500,
-        marginTop: '1%',
-        alignSelf: 'flex-start',
-    },
+  title: {
+    fontWeight: 500,
+    marginTop: "1%",
+    alignSelf: "flex-start",
+  },
 
-    description: {
-        alignSelf: 'flex-start',
-    },
+  description: {
+    alignSelf: "flex-start",
+  },
 
-    button: {
-        backgroundColor:'#405DE6',
-        paddingHorizontal: 10,
-        paddingVertical: 6,
-        textAlign: 'center',
-        borderRadius:4, 
-        borderWidth:1,
-        borderStyle: 'solid',
-        borderColor: '#fff',
-        marginTop: 10,
-    },
+  button: {
+    backgroundColor: "#405DE6",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    textAlign: "center",
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#fff",
+    marginTop: 10,
+  },
 
-    textButton: {
-        color: '#fff',
-    },
+  textButton: {
+    color: "#fff",
+  },
 
-    likeButton: {
-        flex: 1,
-        marginTop: 10,
-        flexWrap: "wrap",
-        alignSelf: 'flex-start',
-    },
+  likeButton: {
+    flex: 1,
+    marginTop: 10,
+    flexWrap: "wrap",
+    alignSelf: "flex-start",
+  },
 
-    user: {
-        marginBottom: 8,
-        alignSelf: "flex-start",
-    },
-
-})
+  user: {
+    marginBottom: 8,
+    alignSelf: "flex-start",
+  },
+});
 
 export default Post;
